70151b1e7b7e125bb526ba62b79e1e6de7837127
elugabriel <gabrielelufidod2016@gmail.com>
2025-05-02 09:34:27 +0100

    User dashboard created

--- run.py
+++ run.py
@@ -7,7 +7,8 @@ from wtforms.validators import DataRequired, Length, Email
 from flask_login import UserMixin
 import os
 from flask_login import LoginManager, login_user, login_required, logout_user, current_user
-from werkzeug.security import generate_password_hash
+from werkzeug.security import check_password_hash, generate_password_hash
+
 
 # === App Config ===
 
@@ -94,6 +95,17 @@ class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Login')
+    
+class UpdateProfileForm(FlaskForm):
+    name = StringField('Full Name', validators=[DataRequired()])
+    phone = StringField('Phone Number', validators=[DataRequired()])
+    dob = DateField('Date of Birth', validators=[DataRequired()])
+    gender = SelectField('Gender', choices=[('Male', 'Male'), ('Female', 'Female')], validators=[DataRequired()])
+    marital_status = SelectField('Marital Status', choices=[('Single', 'Single'), ('Married', 'Married')], validators=[DataRequired()])
+    address = StringField('Address', validators=[DataRequired()])
+    state = StringField('State', validators=[DataRequired()])
+    submit = SubmitField('Update Profile')
+
 
 # === ROUTES ===
 @app.route('/')
@@ -104,49 +116,74 @@ def home():
 def signup_user():
     form = UserSignUpForm()
     if form.validate_on_submit():
+        hashed_password = generate_password_hash(form.password.data)
         new_user = User(
             name=form.name.data,
-            email=form.email.data,
             phone=form.phone.data,
             dob=form.dob.data,
             gender=form.gender.data,
             marital_status=form.marital_status.data,
             address=form.address.data,
             state=form.state.data,
-            password_hash=form.password.data  # Note: Use password hashing in production!
+            email=form.email.data,
+            password_hash=hashed_password  # Hash the password here
         )
         db.session.add(new_user)
         db.session.commit()
         flash('Signup successful!', 'success')
-        return redirect(url_for('home'))
-    return render_template('signup_user.html', form=form)
+        return redirect(url_for('user_login'))
+    return render_template('user_signup.html', form=form)
+
 
 # Setup Flask-Login
 login_manager = LoginManager(app)
-login_manager.login_view = 'login_user'
+login_manager.login_view = 'user_login'
 
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
 
 @app.route('/login', methods=['GET', 'POST'])
-def login_user():
+def user_login():  # <-- Changed name here
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
+
         if user and check_password_hash(user.password_hash, form.password.data):
-            login_user(user)
+            login_user(user)  # Now refers correctly to flask_login.login_user
             flash('Login successful!', 'success')
             return redirect(url_for('user_dashboard'))
         else:
             flash('Login failed. Check your email and/or password.', 'danger')
-    return render_template('login.html', form=form)
+    
+    return render_template('user_login.html', form=form)
+
+
 
 @app.route('/dashboard')
-@login_required  # Only accessible if the user is logged in
+@login_required
 def user_dashboard():
     return render_template('user_dashboard.html', user=current_user)
 
+
+@app.route('/update-profile', methods=['GET', 'POST'])
+@login_required
+def update_profile():
+    form = UpdateProfileForm(obj=current_user)  # Pre-fill with current user data
+
+    if form.validate_on_submit():
+        current_user.name = form.name.data
+        current_user.phone = form.phone.data
+        current_user.dob = form.dob.data
+        current_user.gender = form.gender.data
+        current_user.marital_status = form.marital_status.data
+        current_user.address = form.address.data
+        current_user.state = form.state.data
+        db.session.commit()
+        flash('Your profile has been updated!', 'success')
+        return redirect(url_for('user_dashboard'))
+
+    return render_template('update_profile.html', form=form)
 
 
 
